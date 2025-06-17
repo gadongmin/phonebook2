825d2cfc11dae16b55209b3dbab0c276e7aa759a
김동민 <dmkim777@naver.com>
2025-06-17 13:14:49 +0900

    20250617(1)

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -3,6 +3,7 @@ package com.javaex.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,10 +28,17 @@ public class PhonebookController extends HttpServlet {
 		// DB데이터 가져오기
 		PhonebookDAO phonebookDAO = new PhonebookDAO();
 		List<PersonVO> personList = phonebookDAO.personSelect();
+		
 		System.out.println(personList);
 		
-		// html + list
+		// 저밑에 있는 list.jsp에게 후반일 html을 만들고 응답문서 만들어 보낸다
+		// 1)request객체에 데이터를 넣어준다
+		request.setAttribute("pList", personList);
 		
+		// 2)list.jsp에 request객체와 response객체를 보낸다
+		// forward
+		RequestDispatcher rd = request.getRequestDispatcher("/list.jsp");
+		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
